fa187dfa74f34592925398eeab689469ce914846
Umut Bilal Okur <okur.umutbilal@gmail.com>
2024-06-02 13:56:40 +0300

    login and signup pages are done

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
   imports: [],
   templateUrl: './admin.component.html',
-  styleUrl: './admin.component.css'
+  styleUrl: './admin.component.css',
 })
 export class AdminComponent {
-
+  authService = inject(AuthService);
+  router = inject(Router);
+  public logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  authService = inject(AuthService);
+  router = inject(Router);
+
+  protected loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  })
+
+  onSubmit(){
+    if(this.loginForm.valid){
+      console.log(this.loginForm.value);
+      this.authService.login(this.loginForm.value)
+      .subscribe((data: any) => {
+        if(this.authService.isLoggedIn()){
+          this.router.navigate(['/admin']);
+        }
+        console.log(data);
+      });
+    }
+  }
 
 }
